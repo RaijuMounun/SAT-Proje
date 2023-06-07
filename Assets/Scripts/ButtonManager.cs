9e7252373a1bc7ed0de163917960ef4a43d55592
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2023-06-07 22:26:19 +0300

    Player visual and some shit

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,9 +1,10 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject settingsPanel;
+    [CanBeNull] public GameObject settingsPanel;
     public void StartGame() { SceneManager.LoadScene("Game"); }
 
     public void Settings() { settingsPanel.SetActive(!settingsPanel.activeInHierarchy); }
